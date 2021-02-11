f0c1b9601fbad40821c3f0f081b48bb21c5c0aed
Amer <hadzicamer@outlook.com>
2021-02-11 21:34:08 +0100

    User login action, reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,15 +3,22 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productDetailsReducer, productListReducer} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList:productListReducer,
     productDetails:productDetailsReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    userLogin:userLoginReducer
 })
 const cartItemsStorage=localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[]
+
+const userInfoStorage=localStorage.getItem('userInfo')?JSON.parse(localStorage.getItem('userInfo')):null
+
+
 const initialState={
-    cart:{cartItems:cartItemsStorage}
+    cart:{cartItems:cartItemsStorage},
+    userLogin:{userInfo:userInfoStorage},
 }
 const middleware=[thunk]
 const store=createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
