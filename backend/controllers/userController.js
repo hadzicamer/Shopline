c18213c798753ad429f79394c1d0ffff7023de01
Amer <hadzicamer@outlook.com>
2021-02-11 15:33:42 +0100

    JWT, authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,44 @@
+import asyncHandler from 'express-async-handler'
+import genToken from '../utilities/genToken.js'
+import User from '../models/userModel.js';
+
+
+
+const authUser=asyncHandler(async(req,res)=>{
+    const {email,password}=req.body
+
+    const user=await User.findOne({email})
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            token:genToken(user._id)
+        })
+    } else{
+        res.status(401)
+        throw new Error('Invalid email/password')
+    }
+}) 
+
+const getProfile=asyncHandler(async(req,res)=>{
+const user=await User.findById(req.user._id)
+if(user){
+   res.json({ 
+    _id:user._id,
+    name:user.name,
+    email:user.email,
+    isAdmin:user.isAdmin
+   })
+} 
+else{
+    res.status(404)
+    throw new Error('User not found')
+}
+}) 
+
+export {
+    authUser,getProfile
+}
