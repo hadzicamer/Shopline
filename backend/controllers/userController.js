d8c802af6bb3e0f77926df3b3c86668c11bc596a
Amer <hadzicamer@outlook.com>
2021-02-11 17:36:06 +0100

    User registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,36 @@ const authUser=asyncHandler(async(req,res)=>{
     }
 }) 
 
+const registerUser=asyncHandler(async(req,res)=>{
+    const {name,email,password}=req.body
+
+    const userExists=await User.findOne({email})
+
+  if(userExists){
+      res.status(400)
+      throw new Error('User already exists')
+  }
+  const user=await User.create({
+      name,
+      email,
+      password
+  })
+  if(user){
+      res.status(201).json({
+        _id:user._id,
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        token:genToken(user._id) 
+      })
+  }
+  else{
+      res.status(400)
+      throw new Error('Invalid data')
+
+  }
+}) 
+
 const getProfile=asyncHandler(async(req,res)=>{
 const user=await User.findById(req.user._id)
 if(user){
@@ -40,5 +70,5 @@ else{
 }) 
 
 export {
-    authUser,getProfile
+    authUser,getProfile,registerUser
 }
