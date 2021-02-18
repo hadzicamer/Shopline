5097e099b214cfb16256164f4c58dfd216f784e2
Amer <hadzicamer@outlook.com>
2021-02-18 23:22:09 +0100

    Security issue

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import{ USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_RESET, USER_DETAILS_SUCCESS, USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_FAIL, USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS }from '../constants/userConst'
+import{ USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_RESET, USER_DETAILS_SUCCESS, USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_RESET, USER_LIST_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_FAIL, USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS }from '../constants/userConst'
 
 export const userLoginReducer = (state = { }, action) => {
     switch (action.type) {
@@ -66,6 +66,8 @@ export const userLoginReducer = (state = { }, action) => {
         return { loading: false, users: action.payload };
       case USER_LIST_FAIL:
         return { loading: false, error: action.payload };
+        case USER_LIST_RESET:
+          return{users:[]}
       default:
         return state;
     }
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -6,14 +6,23 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { listUsers } from '../actions/userActions';
 
-const UserListScreen = () => {
+const UserListScreen = ({history }) => {
   const dispatch = useDispatch();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
+      if(userInfo && userInfo.isAdmin){
     dispatch(listUsers());
-  }, [dispatch]);
+      }
+      else{
+          history.push('/login')
+      }
+  }, [dispatch,history]);
   const deleteHandler=(id)=>{
       console.log('fdsfds')
   }
